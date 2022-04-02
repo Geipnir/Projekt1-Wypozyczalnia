25864d9cf91ee377f6a1b3b3d7026baee7906174
Geipnir <101973738+Geipnir@users.noreply.github.com>
2022-04-02 15:27:26 +0200

    Add files via upload

---
+++ Adapter/Adapter/Adapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterPattern
+{
+
+    class Adapter : Interfejs
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string GetRequest(String Marka, String Model, String Kolor)
+
+        {
+            return $"Uzytkownik {this._adaptee.GetSpecificRequest()} posiada samochod o wartosciach: " + Marka + " " + Model + " " + Kolor;
+        }
+    }
+}
