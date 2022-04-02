2adedfaa34f845f5fd101e41a7f275bd69fe8410
Geipnir <101973738+Geipnir@users.noreply.github.com>
2022-04-02 15:30:41 +0200

    Add files via upload

---
+++ ProjektFacade/ProjektFacade/Klasy/Imie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektFacade.Klasy
+{
+    internal class Imie
+    {
+        public void SetImie(string zmiennaimie)
+        {
+            Console.WriteLine("Zapisano imie: "+zmiennaimie);
+        }
+    }
+}
---
+++ ProjektFacade/ProjektFacade/Klasy/Nazwisko.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektFacade.Klasy
+{
+    internal class Nazwisko
+    {
+        public void SetNazwisko(string zmiennanazwisko)
+        {
+            Console.WriteLine("Zapisano nazwisko: "+zmiennanazwisko);
+        }
+    }
+}
